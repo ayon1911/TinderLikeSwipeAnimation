c9c45943b582aee5ce4e34701f0f3881047a5487
ayon <kr_ayon@icloud.com>
2018-11-13 15:37:58 +0100

    stackView reaggenged for landscape orientation

--- TinderLikeSwipeAnimation/Controllers/RegistrationVC.swift
+++ TinderLikeSwipeAnimation/Controllers/RegistrationVC.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class RegistrationVC: UIViewController {
-
+    
+    let gradientLayer = CAGradientLayer()
+    
     let selectPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Select Photo", for: .normal)
@@ -50,24 +52,50 @@ class RegistrationVC: UIViewController {
         button.titleLabel?.font = UIFont(name: "AvenirNext-Medium", size: 24)
         return button
     }()
-
+    
+    lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            fullNameTextField, emailTextField, passwordTextField, registerButton
+            ])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        return stackView
+    }()
+    lazy var overallStackView = UIStackView(arrangedSubviews: [
+        selectPhotoButton, verticalStackView
+        ])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupGradientLayer()
+        setupLayout()
         
-        let stackView = UIStackView(arrangedSubviews: [
-                selectPhotoButton, fullNameTextField, emailTextField, passwordTextField, registerButton
-            ])
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        view.addSubview(stackView)
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 40, bottom: 0, right: 40))
-        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        setupNotificationObservers()
+        setupTapGesture()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        gradientLayer.frame = view.bounds
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if self.traitCollection.verticalSizeClass == .compact {
+            overallStackView.axis = .horizontal
+        } else {
+            overallStackView.axis = .vertical
+        }
     }
     
     fileprivate func setupGradientLayer() {
-        let gradientLayer = CAGradientLayer()
         let topColor = #colorLiteral(red: 0.4078431373, green: 0.4274509804, blue: 0.8784313725, alpha: 1)
         let bottomColor = #colorLiteral(red: 0.5176470588, green: 0.2156862745, blue: 0.4901960784, alpha: 1)
         gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
@@ -75,5 +103,47 @@ class RegistrationVC: UIViewController {
         view.layer.addSublayer(gradientLayer)
         gradientLayer.frame = view.bounds
     }
-
+    
+    
+    fileprivate func setupLayout() {
+        view.addSubview(overallStackView)
+        overallStackView.axis = .vertical
+        overallStackView.spacing = 8
+        overallStackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 40, bottom: 0, right: 40))
+        selectPhotoButton.widthAnchor.constraint(equalToConstant: 275).isActive = true
+        overallStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    fileprivate func setupTapGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    fileprivate func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc fileprivate func handleTap() {
+        self.view.endEditing(true)
+    }
+    
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardFrame = value.cgRectValue
+        print(keyboardFrame)
+        
+        let bottomSpace = view.frame.height - overallStackView.frame.origin.y - overallStackView.frame.height
+        print(bottomSpace)
+        
+        let delta = keyboardFrame.height - bottomSpace + 8
+        self.view.transform = CGAffineTransform(translationX: 0, y: -delta)
+    }
+    
+    
+    @objc fileprivate func handleKeyboardHide() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.transform = .identity
+        }, completion: nil)
+    }
+    
 }
